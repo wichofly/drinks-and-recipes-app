797a6349836a4b884389625b42284d94ab57c046
Mauricio Erazo <antonierazo13@hotmail.com>
2025-04-14 22:59:55 +0200

    feat: update IndexPage to display drink recipes and handle empty state

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,7 +1,25 @@
+import { useAppStore } from '../stores/useAppStore';
+
 const IndexPage = () => {
+  const drinks = useAppStore((state) => state.drinks);
+
+  const hasDrinks = drinks.drinks.length > 0;
+
   return (
     <>
-      <h1>Index</h1>
+      <h1 className="text-6xl font-semibold">Recipes</h1>
+
+      {hasDrinks ? (
+        <>
+          {drinks.drinks.map((drink) => (
+            <p key={drink.idDrink}>{drink.strDrink}</p>
+          ))}
+        </>
+      ) : (
+        <p className="text-center my-10 text-2xl">
+          No results yet, use the form to search for recipes.
+        </p>
+      )}
     </>
   );
 };
