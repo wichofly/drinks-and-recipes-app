4734798946ac48a5df90be9bb1dc253de68535e1
Mauricio Erazo <antonierazo13@hotmail.com>
2025-04-15 00:36:14 +0200

    feat: enhance DrinkCard layout and update IndexPage to improve drink display

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -5,7 +5,24 @@ type DrinkCardProps = {
 };
 
 const DrinkCard = ({ drink }: DrinkCardProps) => {
-  return <h2>{drink.strDrink}</h2>;
+  return (
+    <div className="border border-amber-50 shadow-lg">
+      <div className="overflow-hidden">
+        <img
+          src={drink.strDrinkThumb}
+          alt={`Image of ${drink.strDrink}`}
+          className="hover:scale-110 transition-transform duration-1000 hover:rotate-2"
+        />
+      </div>
+
+      <div className="p-5">
+        <h2 className="text-2xl truncate font-medium">{drink.strDrink}</h2>
+        <button className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-semibold text-white text-lg rounded-md">
+          View Recipe
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DrinkCard;
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -11,11 +11,11 @@ const IndexPage = () => {
       <h1 className="text-6xl font-semibold">Recipes</h1>
 
       {hasDrinks ? (
-        <>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-10">
           {drinks.drinks.map((drink) => (
             <DrinkCard key={drink.idDrink} drink={drink} />
           ))}
-        </>
+        </div>
       ) : (
         <p className="text-center my-10 text-2xl">
           No results yet, use the form to search for recipes.
