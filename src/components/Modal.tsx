94c97513fbaa9571f8a7ab38c60977bb449a4b70
Mauricio Erazo <antonierazo13@hotmail.com>
2025-04-16 20:31:29 +0200

    feat: enhance Modal component to display selected recipe details and ingredients

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,6 +9,28 @@ import { useAppStore } from '../stores/useAppStore';
 const Modal = () => {
   const modal = useAppStore((state) => state.modal);
   const closeModal = useAppStore((state) => state.closeModal);
+  const selectedRecipe = useAppStore((state) => state.selectedRecipe);
+
+  // Dynamically extract ingredient + measure pairs
+  const getIngredientsWithMeasures = () => {
+    const ingredients: { ingredient: string; measure: string }[] = [];
+
+    for (let i = 1; i <= 6; i++) {
+      const ingredient =
+        selectedRecipe[`strIngredient${i}` as keyof typeof selectedRecipe];
+      const measure =
+        selectedRecipe[`strMeasure${i}` as keyof typeof selectedRecipe];
+
+      if (ingredient) {
+        ingredients.push({
+          ingredient,
+          measure: measure ?? '',
+        });
+      }
+    }
+
+    return ingredients;
+  };
 
   return (
     <>
@@ -17,16 +39,31 @@ const Modal = () => {
 
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
           <DialogPanel className="max-w-lg space-y-4 border border-stone-400 bg-white p-12 rounded-lg shadow-lg">
-            <DialogTitle className="font-bold text-center">
-              Deactivate account
+            <DialogTitle className="font-bold text-center text-4xl">
+              {selectedRecipe.strDrink}
             </DialogTitle>
-            <Description>
-              This will permanently deactivate your account
+
+            <img
+              src={selectedRecipe.strDrinkThumb}
+              alt={`View from ${selectedRecipe.strDrink}`}
+              className="mx-auto w-96"
+            />
+
+            <Description className="text-2xl">
+              Ingredients & Measures
             </Description>
-            <p>
-              Are you sure you want to deactivate your account? All of your data
-              will be permanently removed.
-            </p>
+            <ul>
+              {getIngredientsWithMeasures().map(
+                ({ ingredient, measure }, index) => (
+                  <li key={index}>
+                    <span>{ingredient}</span>{' '}
+                    {measure && <span>- {measure}</span>}
+                  </li>
+                )
+              )}
+            </ul>
+
+            <p className="text-lg">{selectedRecipe.strInstructions}</p>
             <div className="flex gap-4">
               <button onClick={closeModal}>Cancel</button>
             </div>
