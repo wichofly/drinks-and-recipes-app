e53857f4fed44ff9bb9ad8b76784ff3e2039b8bd
Mauricio Erazo <antonierazo13@hotmail.com>
2025-04-23 23:47:57 +0200

    feat: add persistence to favorites slice in app store

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,12 +1,12 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 import { createRecipeSlice, RecipeSliceType } from './recipeSlice';
 import { createFavoriteSlice, FavoriteSliceType } from './favoritesSlice';
 
 // This is the main store for the app. It combines all the slices of the store. (https://zustand.docs.pmnd.rs/guides/slices-pattern)
 export const useAppStore = create<RecipeSliceType & FavoriteSliceType>()(
   devtools((...a) => ({
-    ...createRecipeSlice(...a),
-    ...createFavoriteSlice(...a),
+    ...createRecipeSlice(...a), // Non-persistent slice
+    ...persist(createFavoriteSlice, { name: 'favorite-store' })(...a), // Persistent slice
   }))
 );
