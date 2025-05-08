c17577d1250f444364705430741084800f383490
Mauricio Erazo <antonierazo13@hotmail.com>
2025-05-08 22:29:51 +0200

    feat: implement lazy, only loads the components when it´s needed (on demand). Also Suspense is implemented to make loading smoother and less empty

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import IndexPage from './views/IndexPage';
-import FavoritePage from './views/FavoritePage';
 import Layout from './layouts/Layout';
+import { lazy, Suspense } from 'react';
+
+
+// Lazy load the pages to improve performance
+const IndexPage = lazy(() => import('./views/IndexPage'));
+const FavoritePage = lazy(() => import('./views/FavoritePage'));
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/favorites" element={<FavoritePage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/favorites" element={<FavoritePage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
